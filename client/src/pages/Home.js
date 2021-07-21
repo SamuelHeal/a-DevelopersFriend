c779e9dd1c4735958ba2ae23871da9d168dc1041
Samuel Heal <samuelheal@hotmail.com>
2021-07-21 11:36:56 +1000

    login feature working

---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+
+const Home = () => {
+  
+
+  return (
+    <main>
+      <div className="flex-row justify-center">
+      </div>
+    </main>
+  );
+};
+
+export default Home;
