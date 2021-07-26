d47ade2da71b06ae2f34538f726e313751caffdc
Samuel Heal <samuelheal@hotmail.com>
2021-07-26 11:48:53 +1000

    can now add and delete folders from projects

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './Project.css'
 
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
+
 
 import { QUERY_SINGLE_PROJECT} from '../utils/queries';
 import { ADD_FOLDER_TO_PROJECT } from '../utils/mutations';
@@ -12,43 +14,46 @@ import FolderList from '../components/fileLists/FolderList'
 
 
 const SingleProject = () => {
+
+    const [folderName, setFolderName] = useState('')
+    const [characterCount, setCharacterCount] = useState(0)
+
     const { projectID } = useParams()
+
+    const [addFolder, { error }] = useMutation(ADD_FOLDER_TO_PROJECT)
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+    
+        try {
+          const { data } = await addFolder({
+            variables: {
+              folderName,
+              projectID: projectID,
+            },
+          });
+          setFolderName('');
+          setCharacterCount(0)
+          window.location.reload()
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+      const handleChange = (event) => {
+        const { name, value } = event.target;
+    
+        if (name === 'folderName' && value.length <= 30) {
+            setFolderName(value);
+          setCharacterCount(value.length);
+        }
+      };
 
     const { loading, data } = useQuery(QUERY_SINGLE_PROJECT, {
         variables: { projectID: projectID }
     })
 
     const projects = data?.project || {};
-
-    // const folders = projects.folders
-    // const variable = "test"
-
-    // function printFolders() {
-    //     for (let i = 0; i <= folders.length; i++) {
-    //         return (
-    //             <div>
-    //                 <h1>{folders[i].folderName}</h1>
-    //             </div>
-    //         )
-    //     }
-    // }
-
-    // console.log(folders)
-
-    // function test() {
-    //     folders.map((folder) => {
-    //         console.log(folder.folderName)
-    //         console.log(folder._id)
-    //         return (
-    //             <div>
-    //                 <h1>{folder.folderName}</h1>
-    //             </div>
-    //         )
-    //     })
-    // }
-
-    // console.log(test())
-
     
 
     if (loading) {
@@ -60,8 +65,36 @@ const SingleProject = () => {
             <div className='fileContainer'>
                 <h3>Folders</h3>
                 <div className='files'>
-                    <a className='addBtn'>Add Folder</a>
-                    <FolderList folders={projects.folders}/>
+                    <div>
+                    <form className='addFolderForm' onSubmit={handleFormSubmit}>
+                        <div className='formContainer'>
+                            <input 
+                            name='folderName' 
+                            placeholder='Folder Name'
+                            value={folderName}
+                            className='projectInput'
+                            onChange={handleChange}
+                            ></input>
+                        </div>
+                        <p className={`characterCount ${characterCount === 30 || error ? 'text-danger' : ''}`}>
+                        Character Count: {characterCount}/30
+                        </p>
+                        <div>
+                            <button className='button' type='submit'>
+                                Add Folder
+                            </button>
+                        </div>
+                        {error && (
+                        <div className="errorMessage">
+                            {error.message}
+                        </div>
+                        )}
+                    </form>
+                    </div>
+                    <div className='theFolders'>
+                        <FolderList folders={projects.folders}/>
+                    </div>
+                    
 
                 </div>
 
